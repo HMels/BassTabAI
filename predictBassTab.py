ab699498327ca6cde0c0917e4a5c9cc6bbbbcef5
HMels <haboldmels@gmail.com>
2023-03-30 16:47:39 +0200

    Bug fixes

--- predictBassTab.py
+++ predictBassTab.py
@@ -9,21 +9,20 @@ import tensorflow as tf
 import numpy as np
 
 #from Tab2Vec import SkipGramModel
-'''
+
 class MyModel(tf.keras.Model):
-    def __init__(self, embedding_weights, input_sequence, output_sequence):
+    def __init__(self, embedding_weights, output_sequence):
         super(MyModel, self).__init__()
-        self.input_sequence = input_sequence
         self.output_sequence = output_sequence
         #expanded_weights = tf.tile(tf.expand_dims(embedding_weights, axis=0), [4, embedding_weights.shape[0], embedding_weights.shape[1]])
-        #self.embedding_layer = tf.keras.layers.Embedding(
-        #    input_dim=embedding_weights.shape[0], 
-        #    output_dim=embedding_weights.shape[1], 
-        #    weights=embedding_weights, 
-        #    #input_length=input_sequence,
-        #    trainable=False,
-        #    name='embedding'
-        #)
+        self.embedding_layer = tf.keras.layers.Embedding(
+            input_dim=embedding_weights.shape[0], 
+            output_dim=embedding_weights.shape[1], 
+            weights=embedding_weights, 
+            #input_length=input_sequence,
+            trainable=False,
+            name='embedding'
+        )
         self.flatten_layer = tf.keras.layers.Flatten()
         self.dense_layer1 = tf.keras.layers.Dense(256, activation='relu')
         self.dropout_layer1 = tf.keras.layers.Dropout(0.5)
@@ -32,14 +31,13 @@ class MyModel(tf.keras.Model):
         self.output_layer = tf.keras.layers.Dense(output_sequence, activation='softmax')
 
     def call(self, inputs):
-        #embedding = self.embedding_layer(inputs)
-        flattened = self.flatten_layer(inputs)
+        embedding = self.embedding_layer(inputs)
+        flattened = self.flatten_layer(embedding)
         dense1 = self.dense_layer1(flattened)
         dropout1 = self.dropout_layer1(dense1)
         dense2 = self.dense_layer2(dropout1)
         dropout2 = self.dropout_layer2(dense2)
         output = self.output_layer(dropout2)
-        #output = tf.argmax(output, axis=1, output_type=tf.int32) # convert to integers
         return output
 '''
 
@@ -64,9 +62,9 @@ class MyModel(tf.keras.Model):
         lstm = self.lstm_layer(dropout2)
         output = self.output_layer(lstm)
         return output
+'''
 
-
-def train_model(Tokens, sequence_length_input=1, sequence_length_output=1, epochs=10, batch_size=128, learning_rate=0.001):
+def train_model(Tokens, embedding_weights, sequence_length_input=1, sequence_length_output=1, epochs=10, batch_size=128, learning_rate=0.001):
     """
     Trains a neural network using the pre-trained word embeddings on a list of tokenized inputs and targets.
 
@@ -86,7 +84,7 @@ def train_model(Tokens, sequence_length_input=1, sequence_length_output=1, epoch
         A trained TensorFlow model.
     """
     # Initialize the model and loss function.
-    model = MyModel(sequence_length_output)
+    model = MyModel(embedding_weights, 128)
     optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
     #loss_fn = tf.keras.losses.SparseCategoricalCrossentropy()
 
@@ -94,7 +92,8 @@ def train_model(Tokens, sequence_length_input=1, sequence_length_output=1, epoch
     # Prepare the training dataset.
     inputs_ = []
     targets_ = []
-    for input_tokens in Tokens:
+    for Token in Tokens:
+        input_tokens = tf.boolean_mask(Token, Token!=1)
         for i in range(sequence_length_input, input_tokens.shape[0] - sequence_length_output):
             inputs_.append(input_tokens[i-sequence_length_input:i])
             targets_.append(input_tokens[i:i+sequence_length_output])
@@ -109,9 +108,7 @@ def train_model(Tokens, sequence_length_input=1, sequence_length_output=1, epoch
             with tf.GradientTape() as tape:
                 output = model(input_batch)
                 #output = output[:, -sequence_length_output:, :]
-                print("output=",output)
                 target_batch = tf.one_hot(tf.squeeze(target_batch1), depth=output.shape[1])
-                print("inputbatch=",target_batch)
                 loss = tf.reduce_mean(tf.square(output - target_batch))#     tf.keras.losses.CategoricalCrossentropy(output, target_batch)  #           
     
             gradients = tape.gradient(loss, model.trainable_variables)
@@ -128,20 +125,28 @@ with open('tokenized_inputs.pickle', 'rb') as f:
     BasslineLibrary = pickle.load(f)
 
 # Load the pre-trained embeddings
-#embedding_weights = np.load('Embeddings.npy')
+embedding_weights = np.load('Embeddings.npy')
 
 # Train the model.
-model = train_model(BasslineLibrary.Data, sequence_length_input=1, sequence_length_output=1)
+model = train_model(BasslineLibrary.Data, embedding_weights, sequence_length_input=1, sequence_length_output=1)
 
+## TODO fix embedding
+## TODO fix the count, make sure there is a bar every 8th? count?
+## TODO add pauzes
 
 #%% Testing
+i = 188
+input_data = BasslineLibrary.Data[i]
+
 # Use the trained model to make predictions.
-test_input = [0, 1, 1, 1, 5, 1, 1]  # a tokenized input to predict
-test_input = np.array(test_input)
+test_input = tf.boolean_mask(input_data, input_data!=1) #[0,2,2,]  # a tokenized input to predict
+test_input = np.array(test_input[:int(len(test_input)/2)+1])
 output = model.predict(test_input)
 
 prediction = tf.argmax(output, axis=1, output_type=tf.int32).numpy()
 
+print("Input first half of",BasslineLibrary.names[i]+':')
+BasslineLibrary.print_detokenize(tf.boolean_mask(input_data, input_data!=1))
+
+print("Output")
 BasslineLibrary.print_detokenize(np.concatenate([test_input,prediction]))
-
-
